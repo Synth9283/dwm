9d8afb12c61e73b42c382053dd055c0a5d809a2e
Synth <synth@synthwave.tech>
2022-03-02 00:35:19 -0500

    change: sync

--- config.def.h
+++ config.def.h
@@ -62,7 +62,8 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_mint, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 /* custom */
-static const char *maim[] = { "/bin/sh", "-c", "maim -ui $(xdotool selectwindow) | xclip -selection clipboard -t image/png", NULL };
+static const char *maimWindow[] = { "/bin/sh", "-c", "maim -ui $(xdotool selectwindow) | xclip -selection clipboard -t image/png", NULL };
+static const char *maimSelect[] = { "/bin/sh", "-c", "maim -us | xclip -selection clipboard -t image/png", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -100,7 +101,8 @@ static Key keys[] = {
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
     /* custom */
-    { MODKEY|ShiftMask,             XK_s,      spawn,          {.v = maim } },
+    { MODKEY|ShiftMask,             XK_s,      spawn,          {.v = maimWindow } },
+    { MODKEY|ControlMask|ShiftMask, XK_s,      spawn,          {.v = maimSelect } },
 };
 
 /* button definitions */
